de6e9ed84a74d6dcad0e0f9a66379abe2c25727a
TKUIL <TKUIL@protonmail.com>
2024-12-22 23:13:22 +0200

    dataset script location

--- airflow/airflow_home/datasetscript.py
+++ airflow/airflow_home/datasetscript.py
@@ -1,13 +1,17 @@
+import os
 from kaggle.api.kaggle_api_extended import KaggleApi
 
 api = KaggleApi()
 api.authenticate()
 
 try:
+    target_path = os.path.abspath('./dataproject-financial/airflow/airflow_home/dags/data')
+    print(f"Downloading files to: {target_path}")
+
     api.dataset_download_files(
         'stefanoleone992/mutual-funds-and-etfs', 
-        path='./data',
-        unzip=True  # This parameter handles automatic unzipping
+        path=target_path,
+        unzip=True  # Automatically unzips
     )
 except Exception as e:
     print("An error occurred:", e)
